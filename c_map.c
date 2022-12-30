22d103745269de249af8028678f0e166dd4bbdf4
moneebullah25 <58388634+moneebullah25@users.noreply.github.com>
2022-12-30 20:00:24 +0500

    added some improvements to maps

--- c_map.c
+++ c_map.c
@@ -7,11 +7,12 @@ void MapNew(Map* m, unsigned int keysize, unsigned int valuesize)
 	m->alloclen = 4; m->logiclen = 0;
 	m->elems = malloc(m->alloclen * sizeof(MapNode));
 	ASSERT(m->elems != 0);
-	m->elems->keysize = keysize; 
-	m->elems->valuesize = valuesize;
-	m->elems->key = malloc(keysize); 
-	m->elems->value = malloc(valuesize);
-	ASSERT(m->elems->key != 0 && m->elems->value != 0);
+	m->keysize = keysize;
+	m->valuesize = valuesize;
+	// set *key to all 1's with respect to keysize
+	for (unsigned int i = 0; i < m->alloclen; i++)
+		for (unsigned int j = 0; j < m->keysize; j++)	
+			((char*)m->elems[i]->key)[j] = EMPTY;
 }
 
 
@@ -28,7 +29,6 @@ unsigned int HashFunctionInt(void* key, unsigned int keysize)
 	return x;
 }
 
-
 unsigned int HashFunctionStr(void* key, unsigned int keysize)
 {
 	ASSERT(keysize > 0);
@@ -41,10 +41,60 @@ unsigned int HashFunctionStr(void* key, unsigned int keysize)
     return hash;
 }
 
+void SetKeyEmpty(void* key, unsigned int keysize)
+{
+	for (unsigned int j = 0; j < m->keysize; j++)	
+		((char*)key)[j] = EMPTY;
+}
+
+bool isKeyEmpty(void* key, unsigned int keysize)
+{
+	bool result = true;
+	for (unsigned int j = 0; j < m->keysize; j++)	
+		if (((char*)key)[j] != EMPTY) result = false;
+	return result;
+}
+
+void* MapSet(Map* m, void* key, void* value, char* hash)
+{
+	if (m->logiclen == m->alloclen)
+	{
+		// Resize
+		m->alloclen *= 2;
+		m->elems = realloc(m->alloclen * sizeof(MapNode));
+	}
+	ASSERT(key && value);
+	unsigned int hash_value
+	if (hash == "string") hash_value = HashFunctionStr(key, m->keysize);
+	else hash_value = HashFunctionInt(key, m->keysize);
+	hash_value = hash_value%m->alloclen;
+	while !(isKeyEmpty(m->elems[hash_value]->key, m->keysize))
+	{
+		hash_value++;
+	}
+	memory_copy(m->elems[hash_value]->key, key, m->keysize);
+	m->logiclen++;
+}
+
 int main(void)
 {
+	std::map<std::string, int> map;
+	// Insert some values into the map
+	map["one"] = 1;
+	map["two"] = 2;
+	map["three"] = 3;
+
+	Map* m = malloc(sizeof(Map));
+	MapNew(m, sizeof(char*), sizeof(int));
+	MapInsert(m, "Muneeb", 21);
+	MapInsert(m, "Moiz", 15);
+	void* age = MapAt("Moiz");
+	*(int*)age = 16;
+	
+	
 	for (unsigned int i = 1; i < 10000; i += 9)
 	{
+		
 		unsigned int sh, dh;
 		sh = HashFunctionInt(&i, sizeof(unsigned int));
 		dh = HashFunctionInt(&sh, sizeof(unsigned int));
--- c_map.h
+++ c_map.h
@@ -2,23 +2,27 @@
 #define _C_MAP_HEADER_
 
 #include "c_helper.h"
+#include "c_list.h"
+
+#define EMPTY 0xff
+#define DELETED 0xfe
 
 typedef struct {
 	void* key;
 	void* value;
-	unsigned int keysize;
-	unsigned int valuesize;
 } MapNode;
 
 typedef struct {
 	MapNode* elems;
 	unsigned int logiclen;
 	unsigned int alloclen;
+	unsigned int keysize;
+	unsigned int valuesize;
 } Map;
 
 void MapNew(Map* m, unsigned int keysize, unsigned int valuesize);
 unsigned int HashFunctionInt(void* key, unsigned int keysize);
 unsigned int HashFunctionStr(void* key, unsigned int keysize);
-
+void* HashAt(void* key, unsigned 
 
 #endif
