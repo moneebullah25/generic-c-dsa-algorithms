b3245b6c313215dfc42f2fcfacd7ab8bc3f0e086
moneebullah25 <moneebullah25@gmail.com>
2023-01-22 12:32:25 +0500

    wrapper around stdfunc to prevent error

--- src/c_helper.c
+++ src/c_helper.c
@@ -1,115 +1,46 @@
-#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "../includes/c_helper.h"
 
-void FreeString(void *elems)
-{
-	ASSERT(elems);
-	free(*(char **)&elems);
+void FreeString(void *elems) {
+    free(*(char **)elems);
 }
 
-void FreeData(void *elems)
-{
-	ASSERT(elems);
-	free(elems);
+void FreeData(void *elems) {
+    free(elems);
 }
 
-void MemorySwap(void *vp1, void *vp2, unsigned int nbytes)
-{
-	char *buffer = (char *)malloc(nbytes);
-	MemoryCopy(buffer, vp1, nbytes);
-	MemoryCopy(vp1, vp2, nbytes);
-	MemoryCopy(vp2, buffer, nbytes);
-	free(buffer);
+void MemorySwap(void *vp1, void *vp2, size_t nbytes) {
+    char* buffer = malloc(nbytes);
+    memcpy(buffer, vp1, nbytes);
+    memcpy(vp1, vp2, nbytes);
+    memcpy(vp2, buffer, nbytes);
 }
 
-void StringSwap(void *str1, void *str2, unsigned int nbytes)
-{
-	if (str1 == NULL || str2 == NULL) {
-		return;
-	}
-	char *s1 = *(char **)str1;
-	char *s2 = *(char **)str2;
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-	int max_len = (len1 > len2) ? len1 : len2;
-	char *temp = malloc(max_len + 1);
-	StringCopy(&temp, &s1, nbytes);
-	StringCopy(&s1, &s2, nbytes);
-	StringCopy(&s2, &temp, nbytes);
-	free(temp);
+void StringSwap(void *vp1, void *vp2, size_t nbytes) {
+    char *temp = *(char **)vp1;
+    *(char **)vp1 = *(char **)vp2;
+    *(char **)vp2 = temp;
 }
 
-int DataCompare(const void *vp1, const void *vp2, unsigned int nbytes)
-{
-	ASSERT(nbytes > 0);
-	for (unsigned int i = 0; i < nbytes; i++)
-	{
-		if (*((char *)(vp1) + i) != *((char *)(vp2) + i))
-			return (*((char *)(vp1) + i)) - (*((char *)(vp2) + i));
-	}
-	return 0;
+int DataCompare(const void *vp1, const void *vp2, size_t nbytes) {
+    return memcmp(vp1, vp2, nbytes);
 }
 
-int StringCompare(const void *vp1, const void *vp2, unsigned int nbytes)
-{
-	if (vp1 == NULL || vp2 == NULL) {
-		return 0;
-	}
-	const char *s1 = *(const char **)vp1;
-	const char *s2 = *(const char **)vp2;
-	int i = 0;
-	while (i < nbytes)
-	{
-		if (s1[i] != s2[i])
-		{
-			return 1;
-		}
-		i++;
-	}
-	return 0;
+int StringCompare(const void *vp1, const void *vp2, size_t nbytes) {
+    return strcmp(*(char **)vp1, *(char **)vp2);
 }
 
-
-void *MemoryCopy(void *dest, const void *src, unsigned int nbytes)
-{
-	ASSERT(nbytes > 0);
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
-
-	for (unsigned int i = 0; i < nbytes; i++)
-		cdest[i] = csrc[i];
-	return dest;
+void *MemoryCopy(void *dest, const void *src, size_t nbytes) {
+    return memcpy(dest, src, nbytes);
 }
 
-void StringCopy(void *dest, const void *src, unsigned int nbytes)
-{
-	const char *s = *(const char **)src;
-	char *d = *(char **)dest;
-	int i = 0;
-	while (s[i] != '\0')
-	{
-		d[i] = s[i];
-		i++;
-	}
-	d[i] = '\0';
+void StringCopy(void *dest, const void *src, size_t nbytes) {
+    *(char **)dest = strdup(*(char **)src);
 }
 
-void *MemoryMove(void *dest, void *src, unsigned int nbytes)
-{
-	ASSERT(dest != NULL && src != NULL && nbytes > 0);
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
-	// overlap buffer
-	if ((csrc < cdest) && (cdest < csrc + nbytes))
-	{
-		for (cdest += nbytes, csrc += nbytes; nbytes--;)
-			*--cdest = *--csrc;
-	}
-	else
-	{
-		MemoryCopy(dest, src, nbytes);
-	}
-	return dest;
+void *MemoryMove(void *dest, void *src, size_t nbytes) {
+    return memmove(dest, src, nbytes);
 }
