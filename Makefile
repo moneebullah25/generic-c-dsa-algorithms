b64fc0f6351c687be22a24c96a26a20f7e4851d3
moneebullah25 <moneebullah25@gmail.com>
2023-02-05 00:50:50 +0500

    fix some bugs

--- Makefile
+++ Makefile
@@ -16,6 +16,10 @@ OBJS := $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCS))
 # Name of the executable
 EXE := test.out
 
+TEST_DIR=tests
+TESTS=$(wildcard $(TEST_DIR)/*.c)
+TEST_BINS=$(patsubst $(TEST_DIR)/%.c, $(TEST_DIR)/bin/%, $(TESTS))
+
 # Compile all the object files
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(@D)
@@ -23,9 +27,16 @@ $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 
 # Link the object files to create the executable
 $(EXE): test.c $(OBJS) 
-	$(CC) $(CFLAGS) $^ -o $@
+	$(CC) $(CFLAGS) $^ -o $@ -lm
+
+# Create Tests
+$(TEST_DIR)/bin/%: $(TEST_DIR)/%.c $(OBJS)
+	$(CC) $(CFLAGS) $< $(OBJS) -o $@ -lcriterion -lm
 
 # Clean up the directory
 .PHONY: clean
 clean:
-	rm -f $(OBJ_DIR)/*.o $(EXE)
+	rm -f $(OBJ_DIR)/*.o $(EXE) $(TEST_DIR)/bin/*
+
+# Build targets
+all: $(EXE) $(TEST_BINS)
--- tests/t_map.c
+++ tests/t_map.c
@@ -84,3 +84,105 @@ Test(map_test, test_map_string) {
 
 	MapDelete(m);
 }
+
+// Test using minunit
+// #include "minunit.h"
+// #include "../includes/c_map.h"
+// #include <stdlib.h>
+
+// MU_TEST(test_map_int) {
+//     MapInt *m = malloc(sizeof(MapInt));
+//     MapNew(m, HashFunctionInt, LinearProbing, DataCompare, FreeData, FreeData);
+
+//     MapSet(m, 0, 21);
+//     MapSet(m, 2, 15);
+//     MapSet(m, 3, 15);
+//     MapSet(m, 4, 15);
+
+//     int *val = MapGet(m, 2);
+//     mu_assert_int_eq(*val, 15);
+
+//     MapSet(m, 5, 43);
+//     val = MapGet(m, 5);
+//     mu_assert_int_eq(*val, 43);
+
+//     val = MapGet(m, 2);
+//     *val = 16;
+//     val = MapGet(m, 2);
+//     mu_assert_int_eq(*val, 16);
+
+//     MapDelete(m);
+// }
+
+// MU_TEST(test_map_double) {
+//     MapDouble *m = malloc(sizeof(MapDouble));
+//     MapNew(m, HashFunctionInt, LinearProbing, DataCompare, FreeData, FreeData);
+
+//     MapSet(m, 1.0, 21.25);
+//     MapSet(m, 2.0, 15.25);
+//     MapSet(m, 3.0, 15.25);
+//     MapSet(m, 4.0, 15.25);
+
+//     double *val = MapGet(m, 2.0);
+//     mu_assert_double_eq(*val, 15.25);
+
+//     MapSet(m, 5.0, 43.0);
+//     val = MapGet(m, 5.0);
+//     mu_assert_double_eq(*val, 43.0);
+
+//     val = MapGet(m, 2.0);
+//     *val = 16.0;
+//     val = MapGet(m, 2.0);
+//     mu_assert_double_eq(*val, 16.0);
+
+//     MapDelete(m);
+// }
+
+// MU_TEST(test_map_string) {
+// 	MapStringInt *m = malloc(sizeof(Map));
+// 	MapNew(m, HashFunctionStr, LinearProbing, StringCompare, FreeString, FreeData);
+// 	MapSet(m, "Muneeb\0", 21);
+// 	MapSet(m, "Moiz\0", 15);
+// 	MapSet(m, "Moaaz\0", 45);
+// 	MapSet(m, "Arif\0", 75);
+
+// 	MapIter *iter = MapIterator(m);
+// 	int sum = 0;
+// 	while (MapNext(m, iter)) {
+// 		sum += *(int *)iter->node.value;
+// 	}
+// 	mu_assert_int_eq(sum, 156);
+
+// 	MapSet(m, "Kashif\0", 43);
+// 	iter = MapIterator(m);
+// 	sum = 0;
+// 	while (MapNext(m, iter)) {
+// 		sum += *(int *)iter->node.value;
+// 	}
+// 	mu_assert_int_eq(sum, 199);
+
+// 	void *age = MapGet(m, "Moiz\0");
+// 	*(int *)age = 16;
+
+// 	iter = MapIterator(m);
+// 	sum = 0;
+// 	while (MapNext(m, iter)) {
+// 		sum += *(int *)iter->node.value;
+// 	}
+// 	mu_assert_int_eq(sum, 200);
+
+// 	MapDelete(m);
+// }
+
+// MU_RUN_SUITE(test_suite)
+// {
+//     MU_TEST(test_map_int);
+//     MU_TEST(test_map_double);
+//     MU_TEST(test_map_string);
+// }
+
+// int main(int argc, char *argv[]) {
+// 	MU_RUN_SUITE(test_suite);
+// 	MU_REPORT();
+// 	return MU_EXIT_CODE;
+// }
