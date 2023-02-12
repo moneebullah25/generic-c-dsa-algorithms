9803fe8eafa485b124e51c0898dc8e0eb6f9b140
moneebullah25 <moneebullah25@gmail.com>
2023-02-12 12:16:36 +0500

    added some comments

--- includes/c_matrix.h
+++ includes/c_matrix.h
@@ -145,33 +145,340 @@ Space complexity: O(mn), where m is the number of rows and n is the number of co
 Matrix* MatrixCopy(Matrix* m);
 
 /* Matrix Basic */
-bool IsMatrixEqualDimension(Matrix* m1, Matrix* m2);
+/**
+Function that checks if two matrices have equal dimensions.
+The function asserts that the matrix pointers m1 and m2 are not null.
+@param m1 pointer to the first matrix
+@param m2 pointer to the second matrix
+Requirements:
+	- m1 and m2 pointers should not be nullptr
+Time complexity: O(1)
+Space complexity: O(1)
+@return true if the number of rows and columns of the two matrices are equal, false otherwise
+*/
+bool IsMatrixEqualDim(Matrix* m1, Matrix* m2);
+
+/**
+Function that prints a matrix to the console.
+The function asserts that the matrix pointer m and the data format string are not null.
+@param m pointer to the matrix to be printed
+@param data_format string that specifies the format of the matrix data
+Requirements:
+	 - m pointer should not be nullptr
+	 - data_format string should not be nullptr
+Time complexity: O(mn) where m and n are the number of rows and columns of the matrix respectively
+Space complexity: O(1)
+*/
 void PrintMatrix(Matrix* m, const char* data_format);
+
+/**
+Function that checks if a matrix is invertible.
+The function asserts that the matrix pointer m is not null.
+@param m pointer to the matrix
+Requirements:
+	- m pointer should not be nullptr
+Time complexity: O(mn^3) where m and n are the number of rows and columns of the matrix respectively
+Space complexity: O(1)
+@return true if the matrix is invertible, false otherwise
+*/
 bool IsMatrixInvertible(Matrix* m);
 
 /* Matrix Accessing and Modifying */
+/**
+Returns the value stored at the specified row and column in the matrix.
+Time complexity: O(1)
+Space complexity: O(1)
+@param m Pointer to the matrix.
+Requirements:
+	- m pointer should not be nullptr
+@param row Row index of the desired value.
+@param col Column index of the desired value.
+@return The value stored at the specified row and column in the matrix.
+*/
 double MatrixGet(Matrix* m, unsigned row, unsigned col);
+
+/**
+
+Returns a column from the matrix as a new matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(m)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param col Column index to extract from the matrix.
+@return A new matrix representing the specified column from the input matrix.
+*/
 Matrix* MatrixColumnGet(Matrix* m, unsigned int col);
+
+/**
+Returns a row from the matrix as a new matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(n)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param row Row index to extract from the matrix.
+@return A new matrix representing the specified row from the input matrix.
+*/
 Matrix* MatrixRowGet(Matrix* m, unsigned int row);
+
+/**
+Sets the value at the specified row and column in the matrix.
+Time complexity: O(1)
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param row Row index of the value to set.
+@param col Column index of the value to set.
+@param value The value to set at the specified row and column in the matrix.
+*/
 void MatrixSet(Matrix* m, unsigned int row, unsigned int col, double value);
+
+/**
+Sets all values in the matrix to a specified value.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param value The value to set all values in the matrix to.
+*/
 void MatrixAllSet(Matrix* m, double value);
+
+/**
+Sets all diagonal values in the matrix to a specified value.
+Time complexity: O(min(m, n)), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param value The value to set all diagonal values in the matrix to.
+*/
 void MatrixDiagonalSet(Matrix* m, double value);
+
+/**
+Multiplies all values in a specified row of the matrix by a specified value.
+Time complexity: O(n), where n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param row Row index of the values to multiply.
+@param value The value to multiply all values in the specified row of the matrix by.
+*/
 void MatrixRowMultiplyValue(Matrix* m, unsigned int row, double value);
+
+/**
+Add all values in a specified row of the matrix by a specified value.
+Time complexity: O(n), where n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param row Row index of the values to multiply.
+@param value The value to multiply all values in the specified row of the matrix by.
+*/
 void MatrixRowAddValue(Matrix* m, unsigned int row, double value);
+
+/**
+Multiplies all values in a specified column of the matrix by a specified value.
+Time complexity: O(n), where n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param col Column index of the values to multiply.
+@param value The value to multiply all values in the specified row of the matrix by.
+*/
 void MatrixColumnMultiplyValue(Matrix* m, unsigned int col, double value);
+
+/**
+Add all values in a specified column of the matrix by a specified value.
+Time complexity: O(n), where n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param col Column index of the values to multiply.
+@param value The value to multiply all values in the specified row of the matrix by.
+*/
 void MatrixColumnAddValue(Matrix* m, unsigned int col, double value);
+
+/**
+Function that is used to multiply a row to another row (with a multiplicator).
+This will do the following: m->data[where][...] *= m->data[row][...] * value. 
+The results will be kept in a new matrix. Matrix m remains unchanged.
+@param m pointer to the matrix
+@param where index of the row to be multiplied
+@param row index of the row to be multiplied by value
+@param value value to multiply the row by
+Requirements:
+	- m pointer should not be nullptr
+Time complexity: O(n), where n is the number of columns in the matrix
+Space complexity: O(1)
+*/
 void MatrixRowMultiplyRow(Matrix* m, unsigned int where, unsigned int row, double value);
+
+/**
+Function that is used to add a row to another row (with a multiplicator).
+This will do the following: m->data[where][...] += m->data[row][...] * value. 
+The results will be kept in a new matrix. Matrix m remains unchanged.
+@param m pointer to the matrix
+@param where index of the row to be multiplied
+@param row index of the row to be multiplied by value
+@param value value to multiply the row by
+Requirements:
+	- m pointer should not be nullptr
+Time complexity: O(n), where n is the number of columns in the matrix
+Space complexity: O(1)
+*/
 void MatrixRowAddRow(Matrix* m, unsigned int where, unsigned int row, double value);
+
+/**
+Function that is used to multiply a column to another column (with a multiplicator).
+This will do the following: m->data[column][where] *= m->data[...][column] * value. 
+The results will be kept in a new matrix. Matrix m remains unchanged.
+@param m pointer to the matrix
+@param where index of the column to be multiplied
+@param col index of the column to be multiplied by value
+@param value value to multiply the column by
+Requirements:
+	- m pointer should not be nullptr
+Time complexity: O(n), where n is the number of columns in the matrix
+Space complexity: O(1)
+*/
 void MatrixColumnMultiplyColumn(Matrix* m, unsigned int where, unsigned int col, double value);
+
+/**
+Function that is used to add a column to another column (with a multiplicator).
+This will do the following: m->data[column][where] *= m->data[...][column] * value. 
+The results will be kept in a new matrix. Matrix m remains unchanged.
+@param m pointer to the matrix
+@param where index of the column to be multiplied
+@param col index of the column to be multiplied by value
+@param value value to multiply the column by
+Requirements:
+	- m pointer should not be nullptr
+Time complexity: O(n), where n is the number of columns in the matrix
+Space complexity: O(1)
+*/
 void MatrixColumnAddValueColumn(Matrix* m, unsigned int where, unsigned int col, double value);
+
+/**
+Multiplies all values in the matrix with a specified value.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param value The value to set all values in the matrix to.
+*/
 void MatrixWholeMultiply(Matrix* m, double value);
+
+/**
+Add all values in the matrix with a specified value.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+@param m Pointer to the matrix.
+@param value The value to set all values in the matrix to.
+*/
 void MatrixWholeAdd(Matrix* m, double value);
+
+/**
+Remove a specific row from a matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(m-1)n
+Requirements:
+	- m pointer should not be nullptr
+	- row value should be less than the number of rows in the matrix
+@param m Pointer to the matrix.
+@param row The row to be removed from the matrix.
+@return Pointer to the matrix with the specified row removed.
+*/
 Matrix* MatrixRowRemove(Matrix* m, unsigned int row);
+
+/**
+Remove a specific column from a matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(m)(n-1)
+Requirements:
+	- m pointer should not be nullptr
+	- col value should be less than the number of columns in the matrix
+@param m Pointer to the matrix.
+@param col The column to be removed from the matrix.
+@return Pointer to the matrix with the specified column removed.
+*/
 Matrix* MatrixColumnRemove(Matrix* m, unsigned int col);
+
+/**
+Swap two rows of a matrix.
+Time complexity: O(n), where n is the number of columns in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+	- row1 and row2 values should be less than the number of rows in the matrix
+@param m Pointer to the matrix.
+@param row1 The first row to be swapped.
+@param row2 The second row to be swapped.
+*/
 void MatrixRowSwap(Matrix* m, unsigned int row1, unsigned int row2);
+
+/**
+Swap two columns of a matrix.
+Time complexity: O(m), where m is the number of rows in the matrix.
+Space complexity: O(1)
+Requirements:
+	- m pointer should not be nullptr
+	- col1 and col2 values should be less than the number of columns in the matrix
+@param m Pointer to the matrix.
+@param col1 The first column to be swapped.
+@param col2 The second column to be swapped.
+*/
 void MatrixColumnSwap(Matrix* m, unsigned int col1, unsigned int col2);
+
+/**
+Broadcast the row of a matrix to row passed as argument and return that new matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(mn)
+Requirements:
+	- m pointer should not be nullptr
+	- row value that new matrix will have that number of rows
+@param m Pointer to the matrix.
+@param row The number of rows that new broadcasted matrix will have
+@return Pointer to the matrix with the specified row broadcasted to all other rows.
+*/
 Matrix* MatrixBroadcastRows(Matrix* m, unsigned int row);
+
+/**
+Broadcast the column of a matrix to column passed as argument and return that new matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(mn)
+Requirements:
+	- m pointer should not be nullptr
+	- col value that new matrix will have that number of columns
+@param m Pointer to the matrix.
+@param col The number of columns that new broadcasted matrix will have
+@return Pointer to the matrix with the specified row broadcasted to all other rows.
+*/
 Matrix* MatrixBroadcastColumns(Matrix* m, unsigned int col);
+
+/**
+Broadcast the row of a matrix to row passed as argument and return that new matrix. 
+And then Broadcast the column of a matrix to column passed as argument and return that new matrix.
+Time complexity: O(mn), where m is the number of rows and n is the number of columns in the matrix.
+Space complexity: O(mn)
+Requirements:
+	- m pointer should not be nullptr
+	- row value that new matrix will have that number of rows
+	- col value that new matrix will have that number of columns
+@param m Pointer to the matrix.
+@param row The number of rows that new broadcasted matrix will have
+@param col The number of columns that new broadcasted matrix will have
+@return Pointer to the matrix with the specified row broadcasted to all other rows.
+*/
 Matrix* MatrixBroadcastRowsAndColumns(Matrix* m, unsigned int row, unsigned int col);
 
 /* Matrix Operatons */
